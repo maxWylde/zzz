9dee4a16a2252f03895f72640af9dbd6e654ba84
MaximWylde <maxim.gregorian@gmail.com>
2013-11-14 18:48:13 -0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  
+  get "users/new"
+
+  root  'static_pages#home'
+  match '/signup',   to: 'users#new',              via: 'get'
+  match '/help',    to: 'static_pages#help',     via: 'get'
+  match '/about',   to: 'static_pages#about',    via: 'get'
+  match '/contact', to: 'static_pages#contact',  via: 'get'
+  #match '/',        to: 'static_pages#home',     via: 'get'
+  ## I don't know if it's possible to define home route that way
+
+  #get "static_pages/home"
+  #get "static_pages/help"
+  #get "static_pages/about"
+  #get "static_pages/contact"
+
+  #match '/help',    to: 'static_pages#help',    via: 'get'
+  #match '/about',   to: 'static_pages#about',   via: 'get'
+  #match '/contact', to: 'static_pages#contact', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,32 +3,44 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do      
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("RoR Tutorial")
     end
 
     it "should not have the wrong title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end 
   end
   
   describe "Help page Pirate yarrrr purple monkey" do
     it "Should have the content 'Sample App' PIRATES ATTACK YARRR PURPLE" do
-      visit '/static_pages/help'
+      visit help_path
 	  expect(page).to have_content('Sample App')
     end
   end
 
   describe "About page" do
     it "Should tell you something about it" do
-	  visit '/static_pages/about'
+	  visit about_path
       expect(page).to have_content('About')
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content contact" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title contact" do
+      visit contact_path
+      expect(page).to have_title("RoR Tutorial | Contact")
     end
   end
 end
